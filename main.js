7a9ebf8fef79f3af8a6ca6e777db4da9dd167bc7
chidamaps1 <164425796+chidamaps1@users.noreply.github.com>
2024-04-09 16:52:24 +1200

    Update main.js

--- main.js
+++ main.js
@@ -13,53 +13,20 @@ const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/gdmckenzie/clu1okfsi
 }).addTo(map);
 // END REPLACE BLOCK
 
-
-// Custom marker icons
-
-  
-   
-});
-
-var cafeIcon = L.icon({
-    iconUrl: '//file/Usersc$/cch193/Home/Desktop/GIS403 LABS/Symbols/cafepin.png',
-    iconSize: [30,30],
-    iconAnchor: [20,40],
-    popupAnchor: [0, -40]  
-
-});
-
-var resturantIcon = L.icon({
-    iconUrl: '//file/Usersc$/cch193/Home/Desktop/GIS403 LABS/Symbols/restaurant.png',
-    iconSize: [30,30],
-    iconAnchor: [20,40],
-    popupAnchor: [0, -40]  
-});
-
-var cabinIcon = L.icon({
-    iconUrl: '//file/Usersc$/cch193/Home/Desktop/GIS403 LABS/Symbols/cabinpin.png',
-    iconSize: [30,30],
-    iconAnchor: [20,40],
-    popupAnchor: [0, -40]  
-});
-
-//
-
 // There are four markers below.  Each has a latitude and longitude.  Edit these to be your unique places of interest.
 // Also note that each has its own 'pop-up.'  Edit the text in each of these to say why each location is important to you.
 
-const marker1 = L.marker([51.5, -0.09]).addTo(map)
-	.bindPopup('Hello! I am a popup.');
 
 const marker1 = L.marker([-40.335061228045625, 175.6276445697483], .addTo(map)
 	.bindPopup('We, Team Lhotshampa, participate in the annual inter-Nepalese football tournament hosted at Skoglund Park. We \ currently hold the title of five-times champions.');
 
-const marker2 = L.marker([-43.57817117924751, 172.54656944752773], { icon: cafeIcon }).addTo(map)
+const marker2 = L.marker([-43.57817117924751, 172.54656944752773], .addTo(map)
 	.bindPopup('Every morning starts with a Chai Latte here at Suburban Rascal cafe.');
 
-const marker3 = L.marker([-45.86816153383429, 170.506589880616], { icon: resturantIcon }).addTo(map)
+const marker3 = L.marker([-45.86816153383429, 170.506589880616], .addTo(map)
 	.bindPopup("Mela Eatery is one of the best Nepalese resturants in New Zealand. It\'s where I love to go, espically when I'm missing my moms cooking.");
 
-const marker4 = L.marker([-42.49051468800719, 173.16253358475484],{ icon: cabinIcon }).addTo(map)
+const marker4 = L.marker([-42.49051468800719, 173.16253358475484], .addTo(map)
 	.bindPopup('I have spend many weekends at this cabin in Mt Lyford. It\'s a great place to escape with loved ones, offering nearby hiking trails and lakes for exploration.').openPopup();
 
 
